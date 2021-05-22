2e7405c2ad294413df581802d31ccd84490aa89d
unsworthme <23306050+ultramario1998@users.noreply.github.com>
2021-05-22 16:05:26 -0600

    Finished everything!

--- connect.py
+++ connect.py
@@ -1,6 +1,35 @@
 import os
+from google.cloud import storage
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "firebasecloudassignment-f82a5734422d.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "firebasecloudassignment-firebase-adminsdk-credentials.json"	#this json is confidential and isn't included in git
+
+def connect():	#this function returns a db object after connecting with given credentials. we should only ever run it once per session.
+	cred = credentials.ApplicationDefault()
+	firebase_admin.initialize_app(cred, {'projectId': 'firebasecloudassignment'})	#initialize a connection to our project
+	db = firestore.client()
+	return db
 
 def test_connection():
-	pass
+	db = connect()
+	if db:	#if we get anything back, we've probably connected.
+		print("Connection successful.")
+		return db	#this db should be passed around to every other function as needed.
+	else:	#otherwise, inform the user
+		print("Error! Not connected! Recheck credentials and try again.")
+
+def add_name(db, name_dict):
+	location = 'name_' + name_dict['lastname']	#each entry has a unique document, consisting of 'name_' and the last name.
+	test_reference = db.collection(u'names').document(location)	#we tell the db where we're putting data...
+	test_reference.set({	#and proceed to set it.
+		u'firstname': name_dict['firstname'],
+		u'lastname': name_dict['lastname']
+		})
+
+def read_names(db):
+	names_collection = db.collection(u'names')
+	stream = names_collection.stream()	#obtain stream object from our aforedefined collection
+	for x in stream:	#...and iterate over it.
+		print(f'{x.id} => {x.to_dict()}')
--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-import connect.py
+from connect import test_connection
+from connect import add_name
+from connect import read_names
 
-test_connection()
+db = test_connection()
+print("Welcome to the names database.")
+while True:
+	selection = input("Please choose one of the following options:\n1: Input name\n2: Read names\nQuit: Quits the program\n\nPlease input your option: ")
+	# yes, IF-ELSE chains are really super messy, but I'm too lazy to download Python 3.10, which includes a nice switch statement equivalent.
+	if selection == '1':
+		name_dict = {}
+		name_dict[u'firstname'] = input("Enter a first name: ")
+		name_dict[u'lastname'] = input("Enter a last name: ")
+		print("Adding...")
+		add_name(db, name_dict)
+	elif selection == '2':
+		read_names(db)
+	elif selection.lower() == 'quit':
+		break
+	else:
+		print("Invalid choice!\n")
